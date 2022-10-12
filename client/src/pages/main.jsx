50254c782bb2a6e196a8838539e4e54a7eca1e94
Mark Harmon <neothej1sec@gmail.com>
2022-10-11 22:48:46 -0500

    need to research more about localstorage

--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import ItemCard from '../components/ItemCard'
 import ProductData from '../services/ProductData'
-import {readFromFile, writeToFile} from '../services/cart_test/utils'
+import {addItem, getCart} from '../services/cart_test/utils'
 
 const Main = () => {
     // 
-    const data = {
-        "id": 1,
-        "title": "test 2",
-        "content": "texst content 2"
+
+    let data = {
+        id: 2,
+        qty: 3,
+        price: 30
     }
-    writeToFile(data)
-    console.log(readFromFile())
+    let data2 = {
+        id: 4,
+        qty: 2,
+        price: 25
+    }
+    addItem(data)
+    console.log(getCart())
+    
+    // const data = {
+    //     "id": 1,
+    //     "title": "test 2",
+    //     "content": "texst content 2"
+    // }
+    // writeToFile(data)
+    // console.log(readFromFile())
 
     if(ProductData){
         return(
--- client/src/services/cart_test/utils.js
+++ client/src/services/cart_test/utils.js
@@ -1,31 +1,57 @@
 // import {CartData} from './CartData.json'
-// const CartData = require()
-const fs = require('fs')
+const Data = require("./Data.json")
 
-export const writeToFile = (data) => {
-    
-    const file = require('./CartData.json')
-    fs.writeFile(file, JSON.stringify(data))
-}
-export const readFromFile = () => {
-    const data = require('./CartData.json')
-    // JSON.parse(data)
-    return data
-}
+// console.log(Data)
 
 
-export function totalItems(){
-    
-}
-export function addItem(){
+// const total = (data) => {
+//     let start = data.qty
+//     let is_good = true
+//     const sum = 0;
+//     while(is_good){
+//         if (start > 0){
+//             sum.add(parseInt(data.price))
+//             start = start - 1
+//         }else{
+//             is_good = false
+//         }
+//     }
+//     return sum 
+// }
 
-}
-export function getCart(){
 
-}
+// console.log(total(Data))
+// Cart:
+//     Item_ID Qty Price
+
+// export function addItem(item){
+//     const cart = getCart()
+//     cart.push(item)
+//     localStorage.removeItem('cart')
+//     localStorage.setItem('cart', cart)
+//     return cart
+// }
+// // console.log(addItem(data))
+
+// export function getCart(){
+//     try{
+//         const cart = localStorage.getItem('cart')
+//         if(cart){
+//             return cart
+//         }else{
+//             const cart = []
+//             return cart
+//         }
+//     }catch(e){
+//         console.log(e, 'No Cart')
+//     }
+// }
+
+
 export function removeItem(){
 
 }
+
 export function removeAllItems(){
 
 }
