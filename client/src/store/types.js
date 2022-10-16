66dbe032075b72466835379bb1f2dd51fae6c03b
Mark Harmon <neothej1sec@gmail.com>
2022-10-15 20:55:39 -0500

    restarting project with session auth

--- client/src/store/types.js
+++
@@ -1,7 +0,0 @@
-// User Types
-export const GET_USERS = 'GET_USERS'
-export const UPDATE_USER = 'UPDATE_USER'
-export const GET_USER_DETAIL = 'GET_USER_DETAIL'
-export const IS_LOGGED_IN = 'IS_LOGGED_IN'
-export const IS_STAFF = 'IS_STAFF'
-export const GET_PROFILE_DETAIL = 'GET_PROFILE_DETAIL'
