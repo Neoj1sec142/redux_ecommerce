706269ecc67ee8873097b1f1b4daba437fb1539d
Mark Harmon <neothej1sec@gmail.com>
2022-10-17 22:32:30 -0500

    set up cart to refresh from localstorage on page refresh

--- ecommerce/frontend/src/containers/CartContainer.js
+++ ecommerce/frontend/src/containers/CartContainer.js
@@ -11,20 +11,23 @@ const CartContainer = () => {
             }
         }catch(err){console.log(err)}
     },[])
-    
-    return(
-        <div className='card cart'>
-            <h3>Cart</h3>
-            <button className='btn btn-outline-secondary'>Edit Cart</button>
-            <button className='btn btn-outline-secondary'>Check Out</button>
-            <button className='btn btn-outline-danger'>❌</button>
-            <h4>Total: $</h4>
-            <div className='card items'>
-                <h4>Items: </h4>
-                <p>item input(number)increment qty </p>
+    if(items.length){
+        return(
+            <div className='card cart'>
+                <h3>Cart</h3>
+                <button className='btn btn-outline-secondary'>Edit Cart</button>
+                <button className='btn btn-outline-secondary'>Check Out</button>
+                <button className='btn btn-outline-danger'>❌</button>
+                <h4>Total: $</h4>
+                <div className='card items'>
+                    <h4>Items: </h4>
+                    {items.map(item => (
+                    <p>{item.title} Qty:<input type='number'/></p>
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 
 }
 export default CartContainer
