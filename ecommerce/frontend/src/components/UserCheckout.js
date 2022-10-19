44dd5ae4a8e663419b42b8dd513527b67d583794
Mark Harmon <neothej1sec@gmail.com>
2022-10-19 15:47:48 -0500

    notes

--- ecommerce/frontend/src/components/UserCheckout.js
+++ ecommerce/frontend/src/components/UserCheckout.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 //  Purchase Model Fields
 // total, hashed_card, hased_cv, hashed_exp,
 // item_qtys, usr_addr, usr_city, usr_email
-
+//  This page will trigger the hash function securing the userInfo
+//      upon the proceed to confirm chekcout
 const UserCheckout = () => {
     useEffect(() => {
         // load_purchase
@@ -12,7 +13,11 @@ const UserCheckout = () => {
     },[])
     // CHECK UTILS IN HOCS
     return(
-        <div>User Checkout</div>
+        <div>
+            <h1>User Checkout</h1>
+            <button className='btn btn-primary'>Proceed to Confirm Order</button>
+            {/* ^^^ HASH FUNCTION TRIGGERED BY THIS BUTTON ^^^ */}
+        </div>
     )
 }
 export default UserCheckout
--- ecommerce/frontend/src/components/UserConfirmCheckout.js
+++ ecommerce/frontend/src/components/UserConfirmCheckout.js
@@ -1,4 +1,7 @@
 import React from 'react'
+//  After procceed to user info and payment info
+//  This page will NOT trigger the hash function securing the userInfo
+// MAKE SURE TO CHANGE THE CORNFIRM SAVES TO NOT SAVE VULNERABLE INFO
 
 const UserConfirmCheckout = () => {
     return(
