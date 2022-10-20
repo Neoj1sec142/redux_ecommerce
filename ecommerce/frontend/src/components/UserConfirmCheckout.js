d6dae0458c39d785ecddf6a65fbf137116f071d2
Mark Harmon <neothej1sec@gmail.com>
2022-10-20 00:15:25 -0500

    created encryption functions to save user card info

--- ecommerce/frontend/src/components/UserConfirmCheckout.js
+++ ecommerce/frontend/src/components/UserConfirmCheckout.js
@@ -14,23 +14,27 @@ const UserConfirmCheckout = ({
     },[])
 
     return(
-        <div>
-            <h1>Review Your Order:</h1>
-            <p className='text-muted'>{purchase}</p>
-            <h3>Total: ${total}</h3>
-            <h3 className="text">Billing Information:</h3>
-            <article>
-                <h3 className='text-centered'>Billing Info</h3>
-                <p className='text-centered'>
-                    Name: {userInfo.name}
-                    Street: {userInfo.addr}
-                    City: {userInfo.city}
-                    State: {userInfo.state}
-                    Card: {processed.num}
-                    EXP: {processed.exp}
-                    CV: {processed.cv}
-                </p>
-            </article>
+        <div className='container'>
+            <br/>
+            <div className='card'>
+                <h1 className='text-center'>Review Your Order:</h1>
+                <p className='text-muted'>{purchase}</p>
+                <h3 className='text-center'>Total: ${total}</h3>
+            </div>
+            <br/>
+            <div className='card'>
+                <h3 className="text">Billing Information:</h3>
+                <article>
+                    <h5>Name: {userInfo.name}</h5>
+                    <h5>Street: {userInfo.addr}</h5>
+                    <h5>City: {userInfo.city}</h5>
+                    <h5>State: {userInfo.state}</h5>
+                    <h5>Card: {processed.num}</h5>
+                    <h5>EXP: {processed.exp}</h5>
+                    <h5>CV: {processed.cv}</h5>
+                </article>
+            </div>
+            <br/>
             <div className='row'>
                 <div className='col'>
                     <button className='btn btn-primary'>Submit</button>
